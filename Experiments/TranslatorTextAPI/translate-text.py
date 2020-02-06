015e78cb3ce14654d112436f7a822744f30f0d48
Henri Schulte <hens@itu.dk>
2020-02-06 09:17:41 +0100

    Translator is now usable + rosetta stone

--- Experiments/TranslatorTextAPI/translate-text.py
+++ Experiments/TranslatorTextAPI/translate-text.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 def translate(text):
     """Translate given text to Danish. Return translated text."""
+    print('Translating {}...'.format(text[:15]))
 
      # Set request body for request
     body = [{
@@ -47,53 +48,95 @@ headers = {
 # Read dataset
 print('Reading training data...')
 data = []
-with open('data/train_04_feb_test.jsonl') as train_json_file:
+with open('data/train.jsonl') as train_json_file:
     for line in train_json_file:
         line_decoded = json.loads(line)
         data.append(line_decoded)
 
-# Read evidence
-print('Reading evidence...')
-evidence = []
-with open('data/evidence_04_feb_test.jsonl') as evidence_json_file:
-    for line in evidence_json_file:
-        line_decoded = json.loads(line)
-        evidence.append(line_decoded)
+# Read rosetta stone
+print('Reading Rosetta Stone...')
+rosetta_df = pd.read_json('rosetta-stone.json', lines=True)
+
+# Read index
+print('Reading index...')
+index_df = pd.read_json('wiki-index.jsonl', lines=True)
     
 # Convert to dataframe
 train_df = pd.DataFrame(data)
-evidence_df = pd.DataFrame(evidence)
 
 # Sample
-#train_df = train_df.sample(3)
+train_df = train_df.sample(3)
 
 # Loop over claims and translate each
 print('Processing claims...')
 claims_translated = []
-for index, row in train_df.iterrows():
-    # Claim
-    claim = row['claim']
+evidences_translated = []
+evidences_en = []
 
-    claim_da = translate(claim)
+i = 1
+for index, row in train_df.iterrows():
+    print('Processing claim {}/{}...'.format(i, len(train_df['id']+1)), end='\r')
+    i += 1
+    # Claim
+    claim_en = row['claim']
+
+    claim_da = translate(claim_en)
     claims_translated.append(claim_da)
 
     # Evidence
 
-    # # Lookup evidence
-    # claim_evidence_list = row['evidence'][0]
-    # for claim_evidence in claim_evidence_list:
-    #     evidence_id = claim_evidence[2]
-    #     evidence_row = evidence_df.query('id == \'{}\''.format(evidence_id))
-    #     evidence_text = evidence_row['text'].values[0]
+    # Lookup evidence
+    claim_evidence_list = row['evidence'][0]
+    claim_evidence_translated = []
+    claim_evidence_en = []
 
-    #     evidence_text_da = translate(evidence_text)
-    # TODO: Decide what to do with the translated evidence (for now)
+    for claim_evidence in claim_evidence_list:
+        evidence_id = claim_evidence[2]
+        if evidence_id is None:
+            evidence_da = ''
+            evidence_en = ''
+        else:
+            # Determine whether we already translated it before
+            translation_row = rosetta_df.query('id == \'{}\''.format(evidence_id))
+            if len(translation_row['id']):
+                evidence_da = translation_row['evidence_da'].values[0]
+                evidence_en = translation_row['evidence_en'].values[0]
+            else:
+
+                # Look up file name in index
+                index_row = index_df.query('id == \'{}\''.format(evidence_id))
+                file_name = index_row['file_name'].values[0]
+
+                # Look up evidence in file
+                wiki_file = pd.read_json('data/wiki-pages/' + file_name, lines=True)
+                evidence_row = wiki_file.query('id == \'{}\''.format(evidence_id))
+                evidence_en = evidence_row['text'].values[0]
+
+                # Translate evidence
+                evidence_da = translate(evidence_en)
+
+                rosetta_df = rosetta_df.append({'id': evidence_id, 'evidence_da': evidence_da, 'evidence_en': evidence_en}, ignore_index=True)
+
+        claim_evidence_translated.append(evidence_da)
+        claim_evidence_en.append(evidence_en)
+    evidences_translated.append(claim_evidence_translated)
+    evidences_en.append(claim_evidence_en)
+print('')
 
 # Save translations to dataframe
 train_df.insert(4, 'claim_da', claims_translated)
-# print(train_df[['claim', 'claim_da']])
+train_df.insert(5, 'evidence_da', evidences_translated)
+train_df.insert(6, 'evidence_en', evidences_en)
 
-# Save dataframe as csv
-train_df.to_csv('out/{}.csv'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")))
+print('Saving claims and evidence...')
+# Save dataframe as json
+train_df_en = train_df[['id', 'label', 'claim', 'evidence_en']]
+train_df_en.to_json('out/{}_en.jsonl'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
+train_df_da = train_df[['id', 'label', 'claim_da', 'evidence_da']]
+train_df_da.to_json('out/{}_da.jsonl'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
+
+print('Saving rosetta stone...')
+# Save rosetta stone
+rosetta_df.to_json('rosetta-stone.json', orient='records', lines=True)
 
 print("SUCCESS!")
