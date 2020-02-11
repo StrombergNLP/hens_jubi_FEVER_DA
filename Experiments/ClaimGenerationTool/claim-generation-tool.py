0105604409017802887649f125634df837d492d0
Henri Schulte <hens@itu.dk>
2020-02-11 10:24:31 +0100

    Continued claim generation script

--- Experiments/ClaimGenerationTool/claim-generation-tool.py
+++ Experiments/ClaimGenerationTool/claim-generation-tool.py
@@ -1,59 +1,46 @@
 from datetime import datetime
 import pandas as pd
+import random
 
 source_df = pd.read_csv('data/dawiki-latest-abstract.csv')
 source_df = source_df[['title', 'abstract']]
-print('{} rows loaded'.format(len(source_df['title'])))
-
-source_df['title'] = source_df['title'].str.lower()
-
-titles = []
-abstracts = []
-claims = []
-dictionaries = []
-
-while True:
-    print('Source sentence:')
-    source = source_df.sample(1)
-    print(source['abstract'].values)
-    print('')
-
-    user_input = input("Enter claim or write 'skip' or 'quit'\n")
-
-    if user_input == 'skip':
-        continue
-    elif user_input == 'quit':
-        break
-
-    claim = user_input
-
-    user_input = input("Enter other sources (comma separated) or press enter:\n")
-    other_sources = user_input.split(',')
-    other_sources = [x.strip() for x in other_sources]
-
-    dictionary = {}
-    for os in other_sources:
-        if os != '':
-            other_row = source_df.query('title == \'{}\''.format(os.lower()))
-            if len(other_row['title']):
-                other_abstract = ['abstract'].values[0]
-                dictionary[os] = other_abstract
-
-    dictionaries.append(dictionary)
-
-    titles.append(source['title'].values[0])
-    abstracts.append(source['abstract'].values[0])
-    claims.append(claim)
+print('{} rows loaded.\n'.format(len(source_df['title'])))
 
 claims_df = pd.DataFrame()
-claims_df['claim'] = claims
-claims_df['source_title'] = titles
-claims_df['dictionary'] = dictionary
-claims_df['source_abstract'] = abstracts
 
-claims_df.to_json('out/{}.json'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
+while True:
+    source_row = source_df.sample(1) # Sample one row
+    source_title = source_row['title'].values[0]
+    source_abstract = source_row['abstract'].values[0]
+    
+    # Sample one sentence from abstract
+    abstract_split = source_abstract.split('.') # Too imprecise
+    if len(abstract_split):
+        source_sentence = random.choice(abstract_split).strip() + '.'
+    else:
+        continue # Empty abstract, skip
 
-print('Saved {} claims to file.'.format(len(claims_df['claim'])))
+    if len(source_sentence) <= 10:
+        continue # Empty/short sentence, skip
+
+    print('Source entity:')
+    print(source_title)
+    print('Source sentence:')
+    print(source_sentence)
+    print('')
+
+    user_input = input("Enter claim, hit enter to skip or write 'quit'\n")
+
+    if user_input == 'quit':
+        break
+    elif user_input != '': # Add some extra validation here
+        claims_df = claims_df.append({'claim': user_input, 'evidence': source_sentence, 'entity': source_title}, ignore_index=True)
+
+if not claims_df.empty:
+    claims_df.to_json('out/{}.jsonl'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
+    print('Saved {} claims to file.'.format(len(claims_df['claim'])))
+else:
+    print('No claims were saved.')
 
 
 
