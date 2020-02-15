f8bbff580d530df0be360abb11a830c4d60d628b
jubi <jubi@itu.dk>
2020-02-15 10:49:58 +0100

    regex is a bitch ok

--- Experiments/DictionaryExtraction/article-parser.py
+++ Experiments/DictionaryExtraction/article-parser.py
@@ -10,6 +10,7 @@ root = tree.getroot()
 
 df = pd.DataFrame()
 counter = 1
+faults = 0
 
 for index, mother in enumerate(root):
     if mother.tag == 'page':
@@ -22,6 +23,9 @@ for index, mother in enumerate(root):
                 title = child.text
                 title = title.replace(' ', '_')
 
+                if 'Hjælp:' in title or 'Wikipedia:' in title: 
+                    break
+
             elif child.tag == 'revision':
 
                 for grandchild in child:
@@ -29,12 +33,33 @@ for index, mother in enumerate(root):
                         # Handle entire article
                         rawtext = grandchild.text
                         rawtext = re.sub(r"(\[\[File?:.*)|({{.*}})", '',rawtext)
-                        regex = r"(^[\s:]*{{[^}]*}}$)|(&lt;.*?&gt;)|(''')|(^\s*==.*)|<\s*[^>]*>(.*?)<\s*/\s*\w+>"
-                        cleantext = re.sub(regex, '', rawtext, flags=re.MULTILINE|re.DOTALL).strip()
+                        # regex = r"(^[\s:]*{{[^}]*}}$)|(&lt;.*?&gt;)|(''')|(^\s*==.*)|<\s*[^>]*>(.*?)<\s*/\s*\w+>"
+                        # cleantext = re.sub(regex, '', rawtext, flags=re.MULTILINE|re.DOTALL).strip()
 
                         # Save the first line 
-                        abstract = cleantext.splitlines()[0] if len(cleantext.splitlines()) else ''
+                        # abstract = cleantext.splitlines()[0] if len(cleantext.splitlines()) else ''
 
+                        # regex = r"(^[\s:]*{{[^}]*}}$)|(&lt;.*?&gt;)|('''?)|(.*?^}})|(^\s*==.*)|<\s*[^>]*>(.*?)<\s*/\s*\w+>"
+                        regex = r"(^\|.*)|({{[^}]*}*)"
+                        abstract = re.sub(regex, '', rawtext, flags=re.MULTILINE|re.DOTALL).strip()
+
+                        if '#redirect' in abstract.lower():
+                            break
+
+                        # Experiments 
+                        # cleantext = rawtext.splitlines()
+                        #abstract = ''
+
+                        # abstract = cleantext.splitlines()[0] if len(cleantext.splitlines()) else ''
+
+                        # for line in cleantext: 
+                        #     if "'''" in line: 
+                        #         abstract = line 
+                        #         break
+
+                        # regex = r"'''?"
+                        # abstract = re.sub(regex, '', abstract)
+                        
                         # Handle linked entities in that line
                         regex = r"\[\[(.*?)\]\]"
                         links = re.findall(regex, abstract)
@@ -50,9 +75,16 @@ for index, mother in enumerate(root):
                             clean_l = re.sub(r".*(?<!\\)\|", '', l)
                             abstract = abstract.replace('[['+l+']]', clean_l)
 
-                        df = df.append({'Title': title, 'Abstract': abstract, 'Linked Entities': linked_entities}, ignore_index=True)
+                        if abstract != '':
+                            df = df.append({'Title': title, 'Abstract': abstract, 'Linked Entities': linked_entities}, ignore_index=True)
+                        else: 
+                            faults += 1
                         counter +=1
 
+    if counter == 201:
+        break 
+
 print('')
+print('{} faults of {}'.format(faults, counter))
 df.to_json('out/{}.jsonl'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
 print('Saved {} articles to file.'.format(len(df['Title'])))
