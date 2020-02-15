7eafacdfa4b270cc264bbbe3e55cd61d69cc5dea
jubi <jubi@itu.dk>
2020-02-15 10:12:16 +0100

    tried stuff, failed miserably.

--- Experiments/DictionaryExtraction/article-parser.py
+++ Experiments/DictionaryExtraction/article-parser.py
@@ -5,11 +5,13 @@ import re
 
 print('Loading file...')
 tree = ET.parse('data/dawiki-latest-pages-articles-multistream.xml')
-# tree = ET.parse('data/dawiki-test-file.xml')
+# tree = ET.parse('data/dawiki-latest-pages-articles-multistream-sample.xml')
 root = tree.getroot()
 
+print('Loading dataframe...')
 df = pd.DataFrame()
 counter = 1
+faults = 0
 
 for index, mother in enumerate(root):
     if mother.tag == 'page':
@@ -20,6 +22,8 @@ for index, mother in enumerate(root):
 
             if child.tag == 'title':
                 title = child.text
+                if title.startswith('Hjælp:') or title.startswith('Wikipedia:'):
+                    break
                 title = title.replace(' ', '_')
 
             elif child.tag == 'revision':
@@ -28,12 +32,28 @@ for index, mother in enumerate(root):
                     if grandchild.tag == 'text':
                         # Handle entire article
                         rawtext = grandchild.text
-                        rawtext = re.sub(r"(\[\[File?:.*)|({{.*}})", '',rawtext)
-                        regex = r"(^[\s:]*{{[^}]*}}$)|(&lt;.*?&gt;)|(''')|(^\s*==.*)|<\s*[^>]*>(.*?)<\s*/\s*\w+>"
-                        cleantext = re.sub(regex, '', rawtext, flags=re.MULTILINE|re.DOTALL).strip()
+                        regex = r"(^[\s:]*{{[^}]*}}$)|(&lt;.*?&gt;)|(^\s*==.*)|<\s*[^>]*>(.*?)<\s*/\s*\w+>" # |(''')
+                        rawtext = re.sub(regex, '', rawtext, flags=re.MULTILINE|re.DOTALL).strip()
+                        cleantext = rawtext.splitlines()
+                        abstract = ''
 
-                        # Save the first line 
-                        abstract = cleantext.splitlines()[0] if len(cleantext.splitlines()) else ''
+                        for line in cleantext: 
+                            if '=' or '}' not in line and "'''" in line: 
+                                abstract = line
+                                abstract = abstract.replace("'''", '')
+                                break
+                        
+                        if '#redirect' in abstract.lower():
+                            break
+
+                        if abstract == '':
+                            if not len(cleantext):
+                                abstract = ''
+                            elif cleantext[0] != '':
+                                abstract = cleantext[0]
+                            else: 
+                                abstract = cleantext[1]
+
 
                         # Handle linked entities in that line
                         regex = r"\[\[(.*?)\]\]"
@@ -50,9 +70,19 @@ for index, mother in enumerate(root):
                             clean_l = re.sub(r".*(?<!\\)\|", '', l)
                             abstract = abstract.replace('[['+l+']]', clean_l)
 
-                        df = df.append({'Title': title, 'Abstract': abstract, 'Linked Entities': linked_entities}, ignore_index=True)
+                        if abstract != '':
+                            df = df.append({'Title': title, 'Abstract': abstract, 'Linked Entities': linked_entities}, ignore_index=True)
+                        else: 
+                            faults +=1
+
                         counter +=1
 
+    if counter == 10001:
+        break
+
 print('')
+print('{} faults of {} pages'.format(faults, counter))
+print('Finishing parsing...')
+print('Saving to file...')
 df.to_json('out/{}.jsonl'.format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S")), orient='records', lines=True)
 print('Saved {} articles to file.'.format(len(df['Title'])))
