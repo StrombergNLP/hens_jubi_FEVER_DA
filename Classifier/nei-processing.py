13401b41bcc7fbe08bf86886826382a495d11bed
jubi <jubi@itu.dk>
2020-03-11 14:40:45 +0100

    updated NEI script and annotations data

--- Classifier/nei-processing.py
+++ Classifier/nei-processing.py
@@ -9,15 +9,19 @@ def add_nei_evidence():
 def lookup_abstract(entity):
     """ Lookup entity in wiki dataframe and return its abstract in a list """
     row = wiki_df[wiki_df.Title == entity]
-    abstract = row.Abstract.values[0]
+    abstract = row.Abstract.values[0] if len(row.Abstract.values) else ''
     return [abstract]
 
-DATA_PATH = "data/annotations-sample.jsonl"
+print('Reading data...')
+DATA_PATH = "data/annotations.jsonl"
 WIKI_PATH = "data/dawiki-latest-pages-articles-parsed.jsonl"
-
 data_df = pd.read_json(DATA_PATH, lines=True)
 wiki_df = pd.read_json(WIKI_PATH, lines=True)
+print('Reading data complete.')
+
+print('Adding evidence to NEI-labbelled claims...')
 add_nei_evidence()
+print('Adding evidence complete.')
 
 data_df.to_json('data/annotations-filled-nei.jsonl', orient='records', lines=True)
 print('Saved {} claims to file.'.format(len(data_df['claim'])))
