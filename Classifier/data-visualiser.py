194f357061fb2948d726d0408206e737f35e01ee
jubi <jubi@itu.dk>
2020-03-26 09:40:37 +0100

    visuals update

--- Classifier/data-visualiser.py
+++ Classifier/data-visualiser.py
@@ -72,29 +72,86 @@ def avg_matrix(combined_data, config):
     average_matrix = np.average(combined_data, axis=0)
     plot_confusion_matrix(average_matrix, config)
 
+def plot_bars(df, xaxis, yaxis, hue=None):
+    ax = sn.barplot(x=xaxis, y=yaxis, hue=hue, data=df)
+    plt.show()
 
+def scatter_plot(df, xaxis, yaxis):
+    sn.set(style="whitegrid")
+    ax = sn.scatterplot(x=xaxis, y=yaxis, data=df, palette='Blues_d')
+    plt.show()
+
+def plot_precision_recall(c_matrix):
+    refuted, supported, nei = c_matrix[0], c_matrix[1], c_matrix[2]
+
+    true_positives = refuted[0]
+    false_negatives = refuted[1] + refuted[2]
+    false_positives = supported[0] + nei[0]
+    refuted_precision, refuted_recall = calculate_precision_recall(true_positives, false_positives, false_negatives)
+
+    true_positves = supported[1]
+    false_negatives = supported[0] + supported[2]
+    false_positives = refuted[1] + nei[1]
+    supported_precision, supported_recall = calculate_precision_recall(true_positives, false_positives, false_negatives)
+
+    true_positives = nei[2]
+    false_negatives = nei[0] + nei[1]
+    false_positives = refuted[2] + supported[2]
+    nei_precision, nei_recall = calculate_precision_recall(true_positives, false_positives, false_negatives)
+    
+    precision_recall_df = pd.DataFrame(columns=['label', 'attribute', 'value'])
+    precision_recall_df['label'] = ['Refuted', 'Refuted', 'Supported', 'Supported', 'NEI', 'NEI']
+    precision_recall_df['attribute'] = ['precision', 'recall', 'precision', 'recall', 'precision', 'recall']
+    precision_recall_df['value'] = [refuted_precision, refuted_recall, supported_precision, supported_recall, nei_precision, nei_recall]
+
+    plot_bars(precision_recall_df, 'label', 'value', 'attribute')
+
+def calculate_precision_recall(true_positives, false_positives, false_negatives):
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    return precision, recall 
+
+def construct_dataframe(label, att1, att2, att1values, att2values):
+    df = pd.DataFrame(columsn = [label, 'attribute', 'value'])
+    df[label] = 
+    
 #---------
 # Main
 #---------
 
 # Order: [config, c_matrix, loss, micro_f1, macro_f1]
-results_1 = parse_data('out/17-03-2020-14-00-35.json')
-results_2 = parse_data('out/17-03-2020-13-55-47.json')
-results_3 = parse_data('out/17-03-2020-13-52-00.json')
-results_4 = parse_data('out/17-03-2020-13-44-23.json')
-results_5 = parse_data('out/17-03-2020-13-33-09.json')
+results_base = parse_data('results/24-03-2020-13-24-58.json')
 
-# Testing defs
-# plot_confusion_matrix(results_1[1], results_1[0])
-# plot_loss(results_1[2])
-# avg_loss([results_1[2], results_3[2]])
-# avg_matrix([results_1[1], results_3[1], results_2[1], results_4[1], results_5[1]], results_1[0]) 
-micro_df = pd.DataFrame([results_1[3], results_3[3], results_2[3], results_4[3], results_5[3]])
-macro_df = pd.DataFrame([results_1[4], results_3[4], results_2[4], results_4[4], results_5[4]])
-# plot_single_box(micro_df, 'Micro F1')
-# plot_single_box(macro_df, 'Macro F1')
-# plot_multi_box(micro_df, macro_df)
-loss_df = pd.DataFrame(columns=['lr1', 'lr2'])
-loss_df['lr1'] = results_1[2]
-loss_df['lr2'] = results_3[2]
-plot_multi_loss(loss_df)
+results_10percent = parse_data('results/25-03-2020-13-19-45.json')
+results_50percent = parse_data('results/25-03-2020-13-36-03.json')
+results_100percent = parse_data('results/25-03-2020-13-52-21.json')
+
+results_maxlen5 = parse_data('results/25-03-2020-12-29-53.json')
+results_maxlen25 = parse_data('results/25-03-2020-12-22-29.json')
+results_maxlen75 = parse_data('results/25-03-2020-15-05-39.json')
+results_maxlen125 = parse_data('results/25-03-2020-14-48-23.json')
+
+# Incomplete data visuals
+maxlen_df = pd.DataFrame(columns=['max len', 'attribute', 'value'])
+maxlen_df['max len'] = [5, 5, 25, 25, 75, 75, 125, 125, 250, 250]
+maxlen_df['attribute'] = ['micro f1', 'macro f1', 'micro f1', 'macro f1', 'micro f1', 
+                        'macro f1', 'micro f1', 'macro f1', 'micro f1', 'macro f1']
+maxlen_df['value'] = [results_maxlen5[3], results_maxlen5[4], 
+                    results_maxlen25[3], results_maxlen25[4], 
+                    results_maxlen75[3], results_maxlen75[3], 
+                    results_maxlen125[3], results_maxlen125[4], 
+                    results_base[3], results_base[4]]
+plot_bars(maxlen_df, 'max len', 'value', 'attribute')                    
+
+
+
+# Randoms labels visuals
+micro_df = pd.DataFrame(columns=['labels', 'numeric_labels', 'microf1'])
+micro_df['microf1'] = [results_base[3], results_10percent[3], results_50percent[3], results_100percent[3]]
+micro_df['labels'] = ['0%', '10%', '50%', '100%']
+micro_df['numeric_labels'] = [0.0, 0.1, 0.5, 1.0]
+
+# plot_bars(micro_df, 'labels', 'microf1')
+# scatter_plot(micro_df, 'numeric_labels', 'microf1')
+
+# plot_precision_recall(results_50percent[1])
