22d74574a622f3d0d5025d0d96eddc4a97b521eb
jubi <jubi@itu.dk>
2020-03-26 08:06:25 +0100

    content words data

--- Classifier/split-data.py
+++ Classifier/split-data.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
-DATA_PATH = 'data/100percent_random_labels.jsonl'
+DATA_PATH = 'data/replaced_content_words.jsonl'
 data_df = pd.read_json(DATA_PATH, lines=True)
 train_df,validation_df = train_test_split(data_df, test_size=0.1)
 
-train_df.to_json('data/train_data_random_labels_100percent.jsonl', orient='records', lines=True)
-validation_df.to_json('data/validation_random_labels_100percent.jsonl', orient='records', lines=True)
+train_df.to_json('data/train_data_replaced_content_words.jsonl', orient='records', lines=True)
+validation_df.to_json('data/validation_data_replaced_content_words.jsonl', orient='records', lines=True)
