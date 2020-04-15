7cf1cf43bba6b53edae83aeaaa39b294f8e93d20
Henri Schulte <hens@itu.dk>
2020-04-15 13:41:59 +0200

    Dog

--- Document Retrieval/GoldenDocumentRetriever.java
+++ Document Retrieval/GoldenDocumentRetriever.java
@@ -23,7 +23,7 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.IOUtils;
 
-public class LuceneTest {
+public class GoldenDocumentRetriever {
     public static void main(String[] args) throws Exception {
 
         // Building the index
